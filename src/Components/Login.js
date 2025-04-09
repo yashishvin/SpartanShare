d5940ccf0116a70d393b50e97751c1c46dfa2437
yashishvin pothuri <yash@yashishvins-MBP.attlocal.net>
2025-04-09 00:07:29 -0700

    fix login to use the right set of variables

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,25 +1,26 @@
 import GoogleButton from 'react-google-button'
 import { GoogleLogin } from '@react-oauth/google';
-function HandleLogin()
-{
-  console.log('Success logging in')
-}
-function HandleLoginError()
-{
-  console.log('error logging in')
-}
+
 /*
 *TODO:1)Look into callbacks add necessary code for getting info from login
 *     2)Look for alternative method, we need to protect the routes
 *     3)Why did they use fire base ? 
 */
+
+
 function Login()
-{  
+{ 
   return(
     <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: '300px' }}>
             <GoogleLogin
-             onSuccess = { HandleLogin() }
-             onError   = { HandleLoginError() }/>
+              clientId  = {process.env.REACT_APP_GOOGLE_CLIENT_ID}
+              onSuccess={credentialResponse => {
+                console.log(credentialResponse);
+              }}
+              onError={() => {
+                console.log('Login Failed');
+              }}
+            />
     </div>
     );
     
